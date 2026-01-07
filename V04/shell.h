d577b0d7c38b607d426cf4d385547e5e246b0c00
maxim880000 <mdholberton@gmail.com>
2026-01-07 12:49:27 +0100

    committed V04 "Implement the exit built-in, that exits the shell"

---
+++ V04/shell.h
@@ -0,0 +1,24 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define MAX_ARGS 64
+
+extern char **environ;
+
+int execute_command(char *line, char *shell_name);
+char **parse_line(char *line);
+void free_args(char **args);
+char *_getenv(const char *name);
+char *find_in_path(char *command);
+int is_builtin(char *command);
+int execute_builtin(char **args);
+
+#endif
